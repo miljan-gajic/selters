4d08d03c87cf232e2d3d4ac4e9ff0f80710d7b2e
miljan-gajic <miljan.gajic985@gmail.com>
2024-03-09 20:25:20 +0100

    Adding some stuff

--- src/lib/components/NavItems.svelte
+++ src/lib/components/NavItems.svelte
@@ -1,6 +1,26 @@
-<a class="btn btn-sm variant-ghost-surface hover:variant-soft-primary" href="/about"> About Us </a>
-<a class="btn btn-sm variant-ghost-surface hover:variant-soft-primary" href="/about"> Classes </a>
-<a class="btn btn-sm variant-ghost-surface hover:variant-soft-primary" href="/about">
+<a
+	class="btn btn-sm variant-ghost-surface dark:hover:variant-soft-primary hover:variant-soft-secondary"
+	href="/about"
+>
+	About Us
+</a>
+<a
+	class="btn btn-sm variant-ghost-surface dark:hover:variant-soft-primary hover:variant-soft-secondary"
+	href="/about"
+>
+	Classes
+</a>
+<a
+	class="btn btn-sm variant-ghost-surface
+	       dark:hover:variant-soft-primary hover:variant-soft-secondary"
+	href="/about"
+>
 	Tournaments
 </a>
-<a class="btn btn-sm variant-ghost-surface hover:variant-soft-primary" href="/about"> News </a>
+<a
+	class="btn btn-sm variant-ghost-surface
+	       dark:hover:variant-soft-primary hover:variant-soft-secondary"
+	href="/about"
+>
+	News
+</a>
